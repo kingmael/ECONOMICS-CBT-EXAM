218259f0b96f1d4400054676b2139ca40c71f958
AKINSANYA GBENGA MICHAEL <gbengaakinsanya2008@gmail.com>
2018-09-12 13:23:26 +0100

    Update aproject4.py

--- aproject4.py
+++ aproject4.py
@@ -12,7 +12,6 @@ if (question1.lower()== "b"):
     score = score +1
 else:
     print ("wrong          ❌")
-    score = score -1
 
 question2 = input("\n2. An arrangement of data in rows and column is referred to as a? \na. graph \nb. bar chart \nc. pie chart \nd. Table. \n")
 if (question2.lower()== "d"):
@@ -20,7 +19,6 @@ if (question2.lower()== "d"):
     score = score +1
 else:
     print ("wrong          ❌")
-    score = score -1
 
 question3 = input("\n3. The difference between the highest and lowest in a set of data is the? \na. Range \nb. medium \nc. variance \nd. mode. \n")
 if (question3.lower()== "a"):
@@ -28,7 +26,6 @@ if (question3.lower()== "a"):
     score = score +1
 else:
     print ("wrong          ❌")
-    score = score -1
 
 question4 = input("\n4. The graph of the function X = a + bY is? \na. linear \nb. quadratic \nc. cubical \nd. exponential. \n")
 if (question4.lower()== "b"):
@@ -36,7 +33,6 @@ if (question4.lower()== "b"):
     score = score +1
 else:
     print ("wrong          ❌")
-    score = score -1
 
 question5 = input("\n5. The most frequently occurring value in a given data is the? \na. mode \nb. median \nc. mean \nd. range. \n")
 if (question5.lower()== "a"):
@@ -45,7 +41,6 @@ if (question5.lower()== "a"):
     score = score +1
 else:
     print ("wrong          ❌")
-    score = score -1
     
 question6 = input("\n6. The sum of items divided by the number of items the? \na. Frequency \nb. Mean \nc. Median \nd. Mode. \n")
 if (question6.lower()== "b"):
@@ -53,7 +48,6 @@ if (question6.lower()== "b"):
     score = score +1
 else:
     print ("wrong          ❌")
-    score = score -1
     
 question7 = input("\n7. Which of the following is a measure of central tendency? \na. Percentage \nb. Graph \nc. Variance \nd. Median. \n")
 if (question7.lower()== "d"):
@@ -61,7 +55,6 @@ if (question7.lower()== "d"):
     score = score +1
 else:
     print ("wrong          ❌")
-    score = score -1
 
 question8 = input("\n8. Which of the following is not a set of measures of central tendency? \na. Mode and medium \nb. mean and medium \nc. Mean and mode \nd. median and percentage. \n")
 if (question8.lower()== "d"):
@@ -69,7 +62,6 @@ if (question8.lower()== "d"):
     score = score +1
 else:
     print ("wrong          ❌")
-    score = score -1
 
 question9 = input("\n9. Which of the following tools of economic analysis is used when data contains more than one category? \na. Bar charts \nb. Component bar charts \nc. Graphs \nd. Symbolic statements. \n")
 if (question9.lower()== "b"):
@@ -77,7 +69,6 @@ if (question9.lower()== "b"):
     score = score +1
 else:
     print ("wrong          ❌")
-    score = score -1
 
 question10 = input("\n10. Find the mode of the following set of scores 1,1,2,2,2,3,3,4,5,?  \na. 2 \nb. 3 \nc. 4 \nd. 5. \n")
 if (question10.lower()== "a"):
@@ -85,6 +76,5 @@ if (question10.lower()== "a"):
     score = score +1
 else:
     print ("wrong          ❌")
-    score = score -1
 
 print ("Your score for this quiz is ",str(score), " over 10")
